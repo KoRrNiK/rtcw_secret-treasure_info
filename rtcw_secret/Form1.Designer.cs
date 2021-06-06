55ae87573c083c5f73d6a486433e0f0f3dac2fd6
KoRrNiK <koliberr123456789@gmail.com>
2021-06-06 17:27:37 +0200

    Ver. 1.0.0

---
+++ rtcw_secret/Form1.Designer.cs
@@ -0,0 +1,382 @@
+﻿
+namespace rtcw_secret
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.tittleProgram = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.allEnd = new System.Windows.Forms.Label();
+            this.t_all = new System.Windows.Forms.Label();
+            this.t_p = new System.Windows.Forms.Label();
+            this.s_all = new System.Windows.Forms.Label();
+            this.s_p = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.timer7 = new System.Windows.Forms.Timer(this.components);
+            this.timer8 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.tittleProgram);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 33);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Montserrat", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label8.Location = new System.Drawing.Point(5, 18);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 10);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "by KoRrNiK";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(50)))));
+            this.panel3.Location = new System.Drawing.Point(3, 17);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(105, 1);
+            this.panel3.TabIndex = 4;
+            // 
+            // tittleProgram
+            // 
+            this.tittleProgram.AutoSize = true;
+            this.tittleProgram.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tittleProgram.Location = new System.Drawing.Point(3, 3);
+            this.tittleProgram.Name = "tittleProgram";
+            this.tittleProgram.Size = new System.Drawing.Size(104, 15);
+            this.tittleProgram.TabIndex = 3;
+            this.tittleProgram.Text = "RtCW - Secret Info";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(174, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(24, 24);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "⭮";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(204, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 24);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "–";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(230, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(24, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "✖";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(35)))), ((int)(((byte)(0)))));
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.allEnd);
+            this.panel2.Controls.Add(this.t_all);
+            this.panel2.Controls.Add(this.t_p);
+            this.panel2.Controls.Add(this.s_all);
+            this.panel2.Controls.Add(this.s_p);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 33);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(260, 241);
+            this.panel2.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(24, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 30);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "All:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(24, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 30);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Found:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(24, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 30);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "All:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(24, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 30);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Found:";
+            // 
+            // allEnd
+            // 
+            this.allEnd.AutoSize = true;
+            this.allEnd.Font = new System.Drawing.Font("Montserrat Medium", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.allEnd.ForeColor = System.Drawing.Color.Red;
+            this.allEnd.Location = new System.Drawing.Point(12, 190);
+            this.allEnd.Name = "allEnd";
+            this.allEnd.Size = new System.Drawing.Size(28, 20);
+            this.allEnd.TabIndex = 6;
+            this.allEnd.Text = "all";
+            // 
+            // t_all
+            // 
+            this.t_all.AutoSize = true;
+            this.t_all.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.t_all.ForeColor = System.Drawing.Color.Yellow;
+            this.t_all.Location = new System.Drawing.Point(62, 160);
+            this.t_all.Name = "t_all";
+            this.t_all.Size = new System.Drawing.Size(25, 30);
+            this.t_all.TabIndex = 5;
+            this.t_all.Text = "0";
+            // 
+            // t_p
+            // 
+            this.t_p.AutoSize = true;
+            this.t_p.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.t_p.ForeColor = System.Drawing.Color.Yellow;
+            this.t_p.Location = new System.Drawing.Point(98, 135);
+            this.t_p.Name = "t_p";
+            this.t_p.Size = new System.Drawing.Size(25, 30);
+            this.t_p.TabIndex = 4;
+            this.t_p.Text = "0";
+            // 
+            // s_all
+            // 
+            this.s_all.AutoSize = true;
+            this.s_all.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.s_all.ForeColor = System.Drawing.Color.Yellow;
+            this.s_all.Location = new System.Drawing.Point(61, 68);
+            this.s_all.Name = "s_all";
+            this.s_all.Size = new System.Drawing.Size(25, 30);
+            this.s_all.TabIndex = 3;
+            this.s_all.Text = "0";
+            // 
+            // s_p
+            // 
+            this.s_p.AutoSize = true;
+            this.s_p.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.s_p.ForeColor = System.Drawing.Color.Yellow;
+            this.s_p.Location = new System.Drawing.Point(98, 43);
+            this.s_p.Name = "s_p";
+            this.s_p.Size = new System.Drawing.Size(25, 30);
+            this.s_p.TabIndex = 2;
+            this.s_p.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Treasure";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Secrets";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 200;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 500;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 500;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Interval = 500;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // timer7
+            // 
+            this.timer7.Interval = 500;
+            this.timer7.Tick += new System.EventHandler(this.timer7_Tick);
+            // 
+            // timer8
+            // 
+            this.timer8.Interval = 500;
+            this.timer8.Tick += new System.EventHandler(this.timer8_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 274);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RtCW - Info Secret";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label s_p;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label s_all;
+        private System.Windows.Forms.Label t_all;
+        private System.Windows.Forms.Label t_p;
+        private System.Windows.Forms.Label allEnd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label tittleProgram;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Timer timer7;
+        private System.Windows.Forms.Timer timer8;
+    }
+}
+
---
+++ rtcw_secret/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+
+namespace rtcw_secret
+{
+    public partial class Form1 : Form
+    {
+
+        public Mem m = new Mem();
+
+        string nameGame = "WolfSP";
+
+        bool processOpen = false;
+        bool checkLoad = false;
+        int pID = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        int r = 252;
+        int g = 3;
+        int b = 3;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            s_p.Text = "0";
+            s_all.Text = "0";
+            t_p.Text = "0";
+            t_all.Text = "0";
+            allEnd.Text = "";
+
+            timer1.Start();
+
+            timer3.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            processOpen = processOpen = m.OpenProcess(nameGame);
+            pID = m.GetProcIdFromName(nameGame);
+            int runProcess = m.ReadByte(nameGame + ".exe+A4021C");
+
+            if (processOpen)
+            {
+
+                if (runProcess == 0)
+                {
+                    m.CloseProcess();
+                }
+
+                int secretPick = m.ReadInt("qagamex86.dll+5791E8");
+                int secretAll = m.ReadInt("qagamex86.dll+726960");
+                
+                int treasurePick = m.ReadInt("qagamex86.dll+5791EC");
+                int treasureAll = m.ReadInt("qagamex86.dll+726964");
+
+                string nameStage = m.ReadString("cgamex86.dll+5A202D").Replace(".bsp", "");
+                //string cutStage = "cutscene";
+
+
+
+                if(secretPick >= 0) s_p.Text = secretPick.ToString();
+                if(secretAll >= 0) s_all.Text = secretAll.ToString();
+
+                if (treasurePick >= 0) t_p.Text = treasurePick.ToString();
+                if (treasureAll >= 0) t_all.Text = treasureAll.ToString();
+
+                if ((treasureAll != 0 || secretAll != 0) && (treasurePick == treasureAll && secretPick == secretAll)) allEnd.Text = "Everything collected";
+                else if(treasureAll == 0 && secretAll == 0) allEnd.Text = "";
+                else allEnd.Text = "";
+                /*
+                if (m.ReadByte(nameGame + ".exe+A4021C") == 1)
+                {
+                    if (nameStage.Contains(cutStage)) label7.Text = "";
+                    else label7.Text = nameStage;
+                }*/
+               
+
+            }
+            else
+            {
+              
+                string na = "N/A";
+                s_p.Text = na;
+                s_all.Text = na;
+                t_p.Text = na;
+                t_all.Text = na;
+                allEnd.Text = "";
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        int mouseX = 0, mouseY = 0;
+        bool mouseDown;
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.SetDesktopLocation(MousePosition.X - mouseX, MousePosition.Y - mouseY);
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+
+
+        // COLORS TITTLE
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            g += 5;
+            tittleProgram.ForeColor = Color.FromArgb(r, g, b);
+            if (g <= 252)
+            {
+                timer3.Stop();
+                timer4.Start();
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            r -= 5;
+            tittleProgram.ForeColor = Color.FromArgb(r, g, b);
+            if (r <= 3)
+            {
+                timer4.Stop();
+                timer5.Start();
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            b += 5;
+            tittleProgram.ForeColor = Color.FromArgb(r, g, b);
+            if (b >= 252)
+            {
+                timer5.Stop();
+                timer6.Start();
+            }
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            g -= 5;
+            tittleProgram.ForeColor = Color.FromArgb(r, g, b);
+            if (g <= 3)
+            {
+                timer6.Stop();
+                timer7.Start();
+            }
+        }
+
+        private void timer7_Tick(object sender, EventArgs e)
+        {
+            r += 5;
+            tittleProgram.ForeColor = Color.FromArgb(r, g, b);
+            if (r >= 252)
+            {
+                timer7.Stop();
+                timer8.Start();
+            }
+        }
+
+        private void timer8_Tick(object sender, EventArgs e)
+        {
+            b -= 5;
+            tittleProgram.ForeColor = Color.FromArgb(r, g, b);
+            if (b <= 3)
+            {
+                timer8.Stop();
+                timer3.Start();
+            }
+        }
+
+        /* ------------- */
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            mouseX = e.X;
+            mouseY = e.Y;
+        }
+    }
+}
